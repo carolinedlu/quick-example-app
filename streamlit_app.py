514b9acb706aa7232dd30e183539021073e43ab2
carolinedlu <42614552+carolinedlu@users.noreply.github.com>
2022-02-09 12:39:06 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("This is a title!")
+st.header("This is a header!")
+st.subheader("This is a subheader!")
+st.button("This is a button!", key=None, help=None, on_click=None, args=None, kwargs=None, *, disabled=False)
